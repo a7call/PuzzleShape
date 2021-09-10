e42b2f886da149cee02a9c77b0ee003bc95b0be1
Mafia94 <leo-pauldecloitre@hotmail.fr>
2021-09-10 17:50:03 +0200

    Changement forme Player
    -

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -4,8 +4,24 @@ using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
+    private Mesh formeComestible;
+
+    private void Start()
+    {
+        formeComestible = GetComponent<MeshFilter>().mesh;
+    }
     void Update()
     {
         transform.Rotate(new Vector3(50, 30, 45) * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other) 
+    { 
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<MeshFilter>().mesh = formeComestible;
+        }
+    
+    
     }
 }
