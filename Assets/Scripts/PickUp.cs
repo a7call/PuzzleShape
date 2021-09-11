96e06c20712990683461365747e02afa7f0bf08f
Mafia94 <leo-pauldecloitre@hotmail.fr>
2021-09-11 16:44:57 +0200

    Test Respawn
    -

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -5,23 +5,43 @@ using UnityEngine;
 public class PickUp : MonoBehaviour
 {
     private Mesh formeComestible;
+    public Rigidbody comestible;
+    
 
     private void Start()
     {
         formeComestible = GetComponent<MeshFilter>().mesh;
+        /**InstantiateComestible();**/
+        InvokeRepeating("ApparaitreComestible", 0, 5.0f);
     }
     void Update()
     {
         transform.Rotate(new Vector3(50, 30, 45) * Time.deltaTime);
     }
 
-    private void OnTriggerEnter(Collider other) 
-    { 
+    private void OnTriggerEnter(Collider other)
+    {
         if (other.CompareTag("Player"))
         {
             other.GetComponent<MeshFilter>().mesh = formeComestible;
         }
-    
-    
     }
+
+    void ApparaitreComestible()
+    {
+        Rigidbody instance = Instantiate(comestible);
+    }
+    /**void InstantiateComestible()
+    {
+        GameObject comestibleActuel = (GameObject)Instantiate(this.gameObject);
+        comestibleActuel.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+
+        StartCoroutine(WaitForInstantiateComestible());
+    }
+    IEnumerator WaitForInstantiateComestible()
+    {
+        yield return new WaitForSeconds(10.0f);
+        InstantiateComestible();
+    }**/
 }
+
