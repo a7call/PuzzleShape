dc5f22cbd923bd93edbd51311b52de6b2cc2bfa2
Valentin Lopez <valentinlat@hotmail.com>
2021-09-13 13:08:30 +0200

    walls prefabs

--- Assets/Scripts/ShapeSpawner.cs
+++ Assets/Scripts/ShapeSpawner.cs
@@ -32,7 +32,7 @@ public class ShapeSpawner : MonoBehaviour
     IEnumerator RespawnPickUp()
     {
         isRespawning = true;
-        yield return new WaitForSeconds(5.0f);
+        yield return new WaitForSeconds(3.0f);
         currentPickUp.SetActive(true);
         isRespawning = false;
     }
