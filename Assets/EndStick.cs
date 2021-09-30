7b4f7b8dfe615e542f3d9ff242219fc20c5f255b
Valentin Lopez <valentinlat@hotmail.com>
2021-09-30 18:20:16 +0200

    Setting up music

--- Assets/EndStick.cs
+++ Assets/EndStick.cs
@@ -3,17 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
+[RequireComponent(typeof(AudioSource))]
 public class EndStick : MonoBehaviour
 {
+    public Sound[] victorySounds;
+    private AudioSource source;
+    bool hasWon = false;
+    public void Start()
+    {
+        source = GetComponent<AudioSource>();
+        foreach (var s in victorySounds)
+        {
+            s.source = source;
+        }
+           
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !hasWon)
         {
-            if (SceneManager.GetActiveScene().buildIndex + 1 < SceneManager.sceneCountInBuildSettings)
-                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-            else
-                SceneManager.LoadScene(0);
+            hasWon = true;
+            StartCoroutine(GoToNextLevel());
         }
+    }
+
+    IEnumerator GoToNextLevel()
+    {
+        int i = Random.Range(0, victorySounds.Length - 1);
+        source.volume = victorySounds[i].volume;
+        MusicManager.GetInstance().CurrentlyPlayingSound.source.volume /= 2;
+        source.PlayOneShot(victorySounds[i].clip);
+        
+        yield return new WaitForSeconds(victorySounds[i].clip.length);
+
+        MusicManager.GetInstance().CurrentlyPlayingSound.source.volume *= 2;
+
+        if (SceneManager.GetActiveScene().buildIndex + 1 < SceneManager.sceneCountInBuildSettings)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        else
+            SceneManager.LoadScene(0);
     }
 }
